e5bc7b9906d918fa2915c2a7762c2559f3240d5f
Отченко Эдуард Юрьевич <.@itstep.kharkov.ua>
2017-11-10 13:00:43 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(function(){
+	/*
+	$("#orderButton").click(function (e) {
+		//показать подложку
+		$(".modal").show();
+		//показать диалог
+		$(".dialog1").show();
+		e.stopPropagation();
+	})
+
+	$(".modal").click(function () {
+		//показать подложку
+		$(".modal").hide();
+		//показать диалог
+		$(".dialog1").hide();
+	})
+
+*/
+});
